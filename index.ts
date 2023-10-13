ac611f139b97eb6896b59f04ff7c2b4d6bed2827
Jake Hubbard <jacobdhubbard@gmail.com>
2023-10-13 17:08:32 -0400

    Add path routes

--- index.ts
+++ index.ts
@@ -3,8 +3,22 @@ import figlet from "figlet";
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    const body = figlet.textSync("This is my first web server with Bun!");
-    return new Response(body);
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      const body = figlet.textSync("Hello, world!");
+      return new Response(body);
+    }
+
+    if (url.pathname === "/about") {
+      return new Response("About me!");
+    }
+
+    if (url.pathname === "/contact") {
+      return new Response("Contact us");
+    }
+
+    return new Response("Not found", { status: 404 });
   },
 });
 
